3fb3d4f32b85d81ddeb3baaaf34ad506ea1d0477
siki2-uiuc <siki2@illinois.edu>
2022-10-05 17:13:14 +0000

    Refactored rock.html code to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,26 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def play_rock
+  
+   index = rand(3)
+
+   moves = ["rock", "paper", "scissors"] 
+
+   @comp_move = moves.at(index)
+
+   if @comp_move == "rock" 
+    @outcome = "tied" 
+   elsif @comp_move == "paper"
+    @outcome = "lost" 
+   elsif @comp_move == "scissors" 
+    @outcome = "won"
+   end
+
     render({ :template => "game_templates/rock.html.erb" })
   end
   
   def play_paper
+    
     render({ :template => "game_templates/paper.html.erb" })
   end
 
