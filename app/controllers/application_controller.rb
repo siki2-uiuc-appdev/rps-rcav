56c28da7fd585378b5920b47d2c8ed2c2b8906de
siki2-uiuc <siki2@illinois.edu>
2022-10-05 17:32:37 +0000

    general formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,30 +5,27 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def play_rock
-  
-   index = rand(3)
+    index = rand(3)
+    moves = ["rock", "paper", "scissors"]
 
-   moves = ["rock", "paper", "scissors"] 
+    @comp_move = moves.at(index)
 
-   @comp_move = moves.at(index)
-
-   if @comp_move == "rock" 
-    @outcome = "tied" 
-   elsif @comp_move == "paper"
-    @outcome = "lost" 
-   elsif @comp_move == "scissors" 
-    @outcome = "won"
-   end
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
 
     render({ :template => "game_templates/rock.html.erb" })
   end
-  
+
   def play_paper
-    
     render({ :template => "game_templates/paper.html.erb" })
   end
 
   def play_scissors
-    render({ :template => "game_templates/scissors.html.erb"})
+    render({ :template => "game_templates/scissors.html.erb" })
   end
 end
